00711b3f053d2378cd175f9f3769eb0b3b0cea2c
Matt Jackson <mattjackson1@gmail.com>
2023-11-18 15:28:50 +0000

    wip:

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -25,7 +25,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         id="search"
         value={qt}
         onChange={handleChange}
-        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 mb-3 text-sm outline-2 placeholder:text-gray-500"
+        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
