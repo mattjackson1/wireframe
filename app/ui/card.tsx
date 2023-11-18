389578dd24d52e2fea889e4e80aa4ceb6e654fa0
Matt Jackson <mattush@hotmail.com>
2023-11-18 19:06:38 +0000

    wip:

--- app/ui/card.tsx
+++ app/ui/card.tsx
@@ -4,7 +4,7 @@ export function Card({
     children: React.ReactNode;
   }) {
     return (
-      <div className="border shadow hover:shadow-lg p-2">
+      <div className="border shadow hover:shadow-lg p-2 hover:-translate-y-0.5 transition-all ease-in-out duration-200">
         {children}
       </div>
     );
