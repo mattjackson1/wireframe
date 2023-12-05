3a3b76f951cdce852baf356039c626f386e8f831
Matt Jackson <mattjackson1@gmail.com>
2023-12-05 15:48:09 +0000

    wip:

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,5 +1,5 @@
 import AcmeLogo from "@/app/ui/acme-logo";
-import Search from '@/app/ui/search';
+import { Search, ResultsSearch } from '@/app/ui/search';
 
 export default function Header() {
     return (
