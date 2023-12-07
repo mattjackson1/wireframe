bfc4a52bed114739fdb7cccb04b415fec7e71c15
Matt Jackson <mattush@hotmail.com>
2023-12-07 15:47:51 +0000

    wip:

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -96,7 +96,7 @@ export default function DashboardSkeleton() {
         <CardSkeleton />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChartSkeleton />
+        
         <LatestInvoicesSkeleton />
       </div>
     </>
