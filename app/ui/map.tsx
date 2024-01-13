3261c2a6644f8b1ed11adf44e569b0d35efca18c
Matt Jackson <mattush@hotmail.com>
2024-01-13 16:54:32 +0000

    wip

--- app/ui/map.tsx
+++ app/ui/map.tsx
@@ -1,30 +1,41 @@
-'use client';
-
+import React, { useEffect, useState } from 'react';
 import { MapContainer as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
-import 'leaflet-defaulticon-compatibility';
+import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
 
 export default function Map({ latitude, longitude }: { latitude: number; longitude: number }) {
 
+  // Use state to track if it's safe to render
+  const [safeToRender, setSafeToRender] = useState(false);
+
+  // useEffect to check if it's safe to render on the client side
+  useEffect(() => {
+    // Check if window is defined, indicating that the code is running on the client side
+    if (typeof window !== 'undefined') {
+      setSafeToRender(true);
+    }
+  }, []);
+
   return (
-    <LeafletMap
-      className="w-100 h-[500px]"
-      center={[latitude, longitude]}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    // Render LeafletMap only if it's safe to render
+    safeToRender && (
+      <LeafletMap
+        className="w-100 h-[500px]"
+        center={[latitude, longitude]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-      <Marker position={[latitude, longitude]}>
-        <Popup>
-          lat: {latitude}, lng: {longitude}
-        </Popup>
-      </Marker>
-
-    </LeafletMap>
+        <Marker position={[latitude, longitude]}>
+          <Popup>
+            lat: {latitude}, lng: {longitude}
+          </Popup>
+        </Marker>
+      </LeafletMap>
+    )
   );
-}
+};
