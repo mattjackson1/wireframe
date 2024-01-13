a3ade2181ef7ecbe8ac920333dbeb05b537f4899
Matt Jackson <mattush@hotmail.com>
2024-01-13 13:21:13 +0000

    wip: show map on results page

--- app/results/results.tsx
+++ app/results/results.tsx
@@ -10,30 +10,27 @@ export default async function Results() {
   const data = await getData(
     `https://api.openobjects.com/v2/infolink/records?key=6037874de4b0d1e39971ca2e&count=10&query=${query}`
   );
-  return (
-    <>
+  return (       
+    <div className="grid gap-4">
       <h1>Found {data.totalRecords > 0 ? data.totalRecords : ' no matching'} services</h1>
-      <div className="grid gap-4 grid-cols-3">
-        <div className="grid gap-4">
-          {data.records.map(
-            (
-              record: {
-                externalId: string;
-                title: string;
-                public_address_map_postcode: string;
-              },
-              index: Key | null | undefined
-            ) => (
-              <Card key={index}>
-                <Link className="block" href={`service/${record.externalId}`}>
-                  <h2>{record.title}</h2>
-                  {record.public_address_map_postcode}
-                </Link>
-              </Card>
-            )
-          )}
-        </div>
-      </div>
-    </>
+
+      {data.records.map(
+        (
+          record: {
+            externalId: string;
+            title: string;
+            public_address_map_postcode: string;
+          },
+          index: Key | null | undefined
+        ) => (
+          <Card key={index}>
+            <Link className="block" href={`service/${record.externalId}`}>
+              <h2>{record.title}</h2>
+              {record.public_address_map_postcode}
+            </Link>
+          </Card>
+        )
+      )}
+    </div>
   );
 }
