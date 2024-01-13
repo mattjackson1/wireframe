f5420c25ea739a6739ed087182378469eec386a8
Matt Jackson <mattush@hotmail.com>
2024-01-13 13:30:21 +0000

    no marker on results page

--- app/ui/map.tsx
+++ app/ui/map.tsx
@@ -6,7 +6,7 @@ import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
 import 'leaflet-defaulticon-compatibility';
 
-export default function Map({ latitude, longitude }: { latitude: number; longitude: number }) {
+export default function Map({ latitude, longitude, marker }: { latitude: number; longitude: number; marker: boolean }) {
   const markerPosition: LatLngExpression = [latitude, longitude];
 
   return (
@@ -20,11 +20,13 @@ export default function Map({ latitude, longitude }: { latitude: number; longitu
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={markerPosition}>
-        <Popup>
-          lat: {markerPosition[0]}, lng: {markerPosition[1]}
-        </Popup>
-      </Marker>
+      {marker !== false && 
+        <Marker position={markerPosition}>
+          <Popup>
+            lat: {markerPosition[0]}, lng: {markerPosition[1]}
+          </Popup>
+        </Marker>
+      }
     </LeafletMap>
   );
 }
