1e652f2a7115a36b8374e22b584cfd04a53df392
Matt Jackson <mattush@hotmail.com>
2024-01-13 15:26:54 +0000

    wip

--- app/ui/map.tsx
+++ app/ui/map.tsx
@@ -1,12 +1,16 @@
-'use client';
-
+import React from 'react';
 import { MapContainer as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
 import 'leaflet-defaulticon-compatibility';
 
-export default function Map({ latitude, longitude, showMarker }: { latitude: number; longitude: number; showMarker: boolean }) {
-  
+interface MapProps {
+  latitude: number;
+  longitude: number;
+  showMarker: boolean;
+}
+
+export default function Map({ latitude, longitude, showMarker = true }: MapProps): JSX.Element {
   return (
     <LeafletMap
       className="w-100 h-[500px]"
@@ -18,15 +22,14 @@ export default function Map({ latitude, longitude, showMarker }: { latitude: num
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      
-      {showMarker}
 
-      <Marker position={[latitude, longitude]}>
-        <Popup>
-          lat: {latitude}, lng: {longitude}
-        </Popup>
-      </Marker>
-
+      {showMarker && (
+        <Marker position={[latitude, longitude]}>
+          <Popup>
+            lat: {latitude}, lng: {longitude}
+          </Popup>
+        </Marker>
+      )}
     </LeafletMap>
   );
 }
