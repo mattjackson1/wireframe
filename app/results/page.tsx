e04442e842b1eac64e36a3dc41b460b10c7f6c59
Matt Jackson <mattush@hotmail.com>
2024-01-14 11:52:13 +0000

    wip:

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,12 +1,28 @@
-'use client'
+'use client';
 
 import Header from '@/app/ui/results/header';
 import Results from '@/app/results/results';
-import Map from '@/app/ui/map';
 import { ResultsSkeleton } from '@/app/ui/skeletons';
-import { Suspense } from 'react';
+import { Suspense, useMemo } from 'react';
+import dynamic from 'next/dynamic';
   
 export default function Page() {
+  const Map = useMemo(() => dynamic(
+    () => import('@/app/ui/map'),
+    {
+      loading: () => (
+        <button type="button" className="inline-flex items-center px-4 py-2 text-gray transition ease-in-out duration-150">
+        <svg className="motion-safe:animate-spin -ml-1 mr-3 h-5 w-5 text-gray" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        Rendering the map...
+      </button>
+      ),
+      ssr: false
+    }
+  ), []);
+
   return (
     <>
       <Header />
@@ -16,19 +32,12 @@ export default function Page() {
             <Results />
           </Suspense>
 
-          <Suspense fallback={
-            <button type="button" className="inline-flex items-center px-4 py-2 text-gray transition ease-in-out duration-150">
-              <svg className="motion-safe:animate-spin -ml-1 mr-3 h-5 w-5 text-gray" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-              </svg>
-              Rendering the map...
-            </button>
-          }>
-            <div className="col-span-2">
-              No Geo-data to plot onto map...
-            </div>
-          </Suspense>
+        
+          <div className="col-span-2">
+            <Suspense>
+              <Map latitude={52.2} longitude={0.75} zoom={10} />
+            </Suspense>
+          </div>
         </div>
       </main>
     </>
--- app/ui/map.tsx
+++ app/ui/map.tsx
@@ -1,30 +1,30 @@
 'use client';
 
 import { MapContainer as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
-import { LatLngExpression } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css'; // Re-uses images from ~leaflet package
 import 'leaflet-defaulticon-compatibility';
 
-export default function Map({ latitude, longitude }: { latitude: number; longitude: number }) {
-  const markerPosition: LatLngExpression = [latitude, longitude];
+export default function Map({ latitude, longitude, zoom = 13 }: { latitude: number; longitude: number; zoom: number }) {
 
   return (
     <LeafletMap
       className="w-100 h-[500px]"
-      center={markerPosition}
-      zoom={13}
+      center={[latitude, longitude]}
+      zoom={zoom}
       scrollWheelZoom={false}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={markerPosition}>
-        <Popup>
-          lat: {markerPosition[0]}, lng: {markerPosition[1]}
-        </Popup>
-      </Marker>
+      {zoom === 13 && (
+        <Marker position={[latitude, longitude]}>
+          <Popup>
+            lat: {latitude}, lng: {longitude}
+          </Popup>
+        </Marker>
+      )}
     </LeafletMap>
   );
 }
