1ada2e644f54c965cf95bb13bbfb7c5b13780a47
Matt Jackson <mattjackson1@gmail.com>
2024-03-14 16:01:40 +0000

    wip:

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 import Header from '@/app/ui/header';
 import Footer from '@/app/ui/footer';
-import Link from 'next/link'
+import Link from 'next/link';
  
 export default function NotFound() {
   return (
@@ -10,7 +10,7 @@ export default function NotFound() {
         <div className="bg-blue-100 text-center p-4">
             <h1 className="font-bold">404</h1>
             <h2 className="text-2xl font-semibold mb-2">uh oh! That page has not been found in this site</h2>
-            <p className="text-lg mb-5">Sorry, we couldn't find requested resource...</p>
+            <p className="text-lg mb-5">Sorry, we couldn&apos;t find requested resource...</p>
 
             <Link href="/">Why not return to the home page?</Link>
         </div>        
