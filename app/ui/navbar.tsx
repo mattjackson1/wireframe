ecb961aa7132b9156beb7f67170f7dce868e44b3
Matt Jackson <mattjackson1@gmail.com>
2024-03-22 16:16:19 +0000

    wip

--- app/ui/navbar.tsx
+++ app/ui/navbar.tsx
@@ -1,5 +1,7 @@
-import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
+'use client';
+
+import { useState } from 'react';
+import { Bars3Icon } from '@heroicons/react/20/solid';
 
 const menu = [
     { name: 'Home', url: '\\' },
@@ -7,19 +9,33 @@ const menu = [
 ];
 
 export default function Navbar() {
+    const [isOpen, setOpen] = useState(false);
+    const toggleMenu = () => setOpen(!isOpen);
+
     return (
-        <nav className="flex flex-row">
-            <ul className="flex gap-5 justify-center">
-                {menu.map((item, index) => (
-                    <a
-                        href={item.url}
-                        key={index}
-                        className="flex items-center hover:underline underline-offset-4"
-                    >
-                        {item.name}
-                    </a>
-                ))}
-            </ul>
-        </nav>
+        <>
+            <button
+                onClick={toggleMenu}
+                className="border p-1 m-2 md:hidden"
+            >
+                <Bars3Icon className="h-[32px] w-[32px]" />
+            </button>            
+            <nav className="flex flex-row">
+                            
+                <ul
+                    className={(!isOpen && "hidden md:flex ") + "fixed top-0 left-0 right-0  md:relative gap-5 bg-white justify-center "}
+                >
+                    {menu.map((item, index) => (
+                        <a
+                            href={item.url}
+                            key={index}
+                            className={(isOpen && "w-100 p-2 ") + "flex items-center hover:underline underline-offset-4"}
+                        >
+                            {item.name}
+                        </a>
+                    ))}
+                </ul>
+            </nav>
+        </>
     )
 }
