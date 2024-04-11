d97f6cb450ff29dec21f02f5901755737ac672eb
Matt Jackson <mattjackson1@gmail.com>
2024-04-11 11:17:42 +0100

    postcode field

--- app/lib/display-fields.ts
+++ app/lib/display-fields.ts
@@ -9,7 +9,7 @@ export const fields: Field[] = [
     { name: 'contact_position', label: 'Position' },
     { name: 'contact_telephone', label: 'Telephone' },
     { name: 'contact_email', label: 'Email', link_type: 'mailto' },
-    { name: 'venue_postcode', label: 'Postcode' },
+    { name: 'public_address_postcode', label: 'Postcode' },
     { name: 'ecd_vacancies_immediate', label: 'Immediate vacancies' },
     { name: 'ecd_sp_wheelchairaccess', label: 'Wheelchair access' },
     { name: 'ecd_sp_cultural', label: 'Cultural provisions' },
