400a4157152109524752a0157173d607b5473475
Matt Jackson <mattjackson1@gmail.com>
2024-04-16 14:21:30 +0100

    404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,21 +1,20 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import puppy from '@/app/ui/images/404-puppy.png';
 
 export default function NotFound() {
     return (
         <>
             <main className="flex flex-col">
-                <div className="bg-blue-100 p-4 text-center">
-                    <h1 className="font-bold">Page not found</h1>
-                    <h2 className="mb-2 text-2xl font-semibold">
-                        Sorry, the page or document you&apos;re looking for is
-                        not available
-                    </h2>
-                    <p className="mb-5 text-lg">
-                        You can try using the search bar or the menu to find
-                        what you&apos;re looking for.
+                <div className="grid grid-cols-1 justify-center gap-3 bg-blue-100 p-4 text-center">
+                    <h1 className="font-bold">Error 404 - Page not found</h1>
+                    <p className="font-semibold">Nothing to see here... please move along.</p>
+                    <p className="font-semibold">
+                        <Link className="underline" href="/">
+                            Why not return to the home page?
+                        </Link>
                     </p>
-
-                    <Link href="/">Why not return to the home page?</Link>
+                    <Image className="mx-auto" src={puppy} alt="A puppy sitting amongst a torn up cushion it has shredded" />
                 </div>
             </main>
         </>
