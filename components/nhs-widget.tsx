b44801cf86ebee0233963793145c91ddb593821e
Matt Jackson <mattjackson1@gmail.com>
2024-04-30 14:30:11 +0100

    wip:

--- components/nhs-widget.tsx
+++ components/nhs-widget.tsx
@@ -1,6 +1,6 @@
 export default function NHSWidget({ uid }: { uid: string }) {
     return (
-        <section className="mt-4 rounded bg-white p-3">
+        <section className="mt-4 rounded bg-white p-3 dark:bg-gray-800">
             <h2 className="mb-3 font-semibold">Access Your Local NHS Services</h2>
             <iframe
                 src={`https://developer.api.nhs.uk/widgets/services/all/?uid=${uid}`}
