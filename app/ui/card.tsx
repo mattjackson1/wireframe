27f0ab2f7f7632644daa1d5a21f477087ca8dc24
Matt Jackson <matt.jackson@idoxgroup.com>
2024-05-24 16:03:31 +0100

    wip:

--- app/ui/card.tsx
+++ app/ui/card.tsx
@@ -1,6 +1,11 @@
-export function Card({ children }: { children: React.ReactNode }) {
+export function Card({ children, className }: { children: React.ReactNode; className?: String }) {
     return (
-        <div className="rounded-lg border bg-gradient-to-t from-indigo-100 to-indigo-200 p-2 shadow transition-all duration-200 ease-in-out hover:-translate-y-0.5 hover:shadow-lg dark:from-gray-800 dark:to-black">
+        <div
+            className={
+                'rounded border bg-gradient-to-t from-indigo-100 to-indigo-200 p-2 shadow transition-all duration-200 ease-in-out hover:-translate-y-0.5 hover:shadow-lg dark:from-gray-800 dark:to-black ' +
+                className
+            }
+        >
             {children}
         </div>
     );
