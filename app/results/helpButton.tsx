ac8eada27239ff8e6116eb5939fc9c00febf630e
Matt Jackson <matt.jackson@idoxgroup.com>
2024-06-03 16:24:36 +0100

    bug: unescaped chars.

--- app/results/helpButton.tsx
+++ app/results/helpButton.tsx
@@ -14,11 +14,11 @@ export default function HelpButton() {
         >
             <h2>Limited results</h2>
             <p>
-                For clarity, we only plot 10 results at a time. You can change the ordering and view the next 10 results by selecting &quot;Next
-                page&quot;
+                For clarity, we only plot 10 results at a time. You can change the ordering and view the next 10 results by selecting &ldquo;Next
+                page&rdquo;
             </p>
             <h2>The map</h2>
-            <p>Set the centre of the map as your currect location, select the "Center on my Current Location" button</p>
+            <p>Set the centre of the map as your currect location, select the &ldquo;Center on my Current Location&rdquo; button</p>
             <p>As you zoom and drag the map, your results will change to reflect this</p>
             <h2>Draw search area</h2>
             <p>You can draw a box on the map to filter your results by location</p>
