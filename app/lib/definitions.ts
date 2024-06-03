47ee64c3af3fdb7a211a6322d956de64f9801bbe
Matt Jackson <matt.jackson@idoxgroup.com>
2024-06-03 11:47:31 +0100

    bug: definitions syntax

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -2,15 +2,15 @@
 // It describes the shape of the data, and what data type each property should accept.
 // For simplicity, we're manually defining these types.
 // However, these types are generated automatically if you're using an ORM such as Prisma.
-export type Record {
+export type Record = {
     externalId: string;
     title: string;
     public_address_map_postcode: string;
     contact_telephone: string;
     contact_email: string;
-}
+};
 
-export type ResultsProps {
+export type ResultsProps = {
     query: string;
     startIndex?: string;
-}
+};
