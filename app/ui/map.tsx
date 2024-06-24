c17ebf682d79f91b3eef347a4a70cd8e8dcfa685
Matt Jackson <matt.jackson@idoxgroup.com>
2024-06-24 11:27:32 +0100

    Home icon

--- app/ui/map.tsx
+++ app/ui/map.tsx
@@ -16,6 +16,11 @@ const Blue_MARKER = `data:image/svg+xml;utf8,${encodeURIComponent(`<?xml version
         <path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"></path>
     </svg>
     `)}`;
+const Home_MARKER = `data:image/svg+xml;utf8,${encodeURIComponent(`<?xml version="1.0" encoding="iso-8859-1"?>
+    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="currentColor" height="200px" width="200px" viewBox="0 0 576 512">
+        <path d="M543.8 287.6c17 0 32-14 32-32.1c1-9-3-17-11-24L512 185V64c0-17.7-14.3-32-32-32H448c-17.7 0-32 14.3-32 32v36.7L309.5 7c-6-5-14-7-21-7s-15 1-22 8L10 231.5c-7 7-10 15-10 24c0 18 14 32.1 32 32.1h32v69.7c-.1 .9-.1 1.8-.1 2.8V472c0 22.1 17.9 40 40 40h16c1.2 0 2.4-.1 3.6-.2c1.5 .1 3 .2 4.5 .2H160h24c22.1 0 40-17.9 40-40V448 384c0-17.7 14.3-32 32-32h64c17.7 0 32 14.3 32 32v64 24c0 22.1 17.9 40 40 40h24 32.5c1.4 0 2.8 0 4.2-.1c1.1 .1 2.2 .1 3.3 .1h16c22.1 0 40-17.9 40-40V455.8c.3-2.6 .5-5.3 .5-8.1l-.7-160.2h32z"></path>
+    </svg>
+    `)}`;
 
 const DefaultIcon = L.icon({
     iconUrl: Red_MARKER,
@@ -24,8 +29,14 @@ const DefaultIcon = L.icon({
     popupAnchor: [0, 0],
 });
 
+const HomeIcon = L.icon({
+    iconUrl: Home_MARKER,
+    iconSize: [16, 16],
+});
+
 export default function Map({ latitude, longitude, zoom = 13 }: MapProps) {
     const [center, setCenter] = useState([latitude, longitude]);
+    const [home, setHome] = useState([]);
 
     const interactiveMap = zoom !== 13;
 
@@ -37,10 +48,11 @@ export default function Map({ latitude, longitude, zoom = 13 }: MapProps) {
                 (position) => {
                     const { latitude, longitude } = position.coords;
                     setCenter([latitude, longitude]);
+                    setHome([latitude, longitude]);
                     map.setView([latitude, longitude], map.getZoom());
                 },
                 (error) => {
-                    console.error('Browser encountered error fetching geolocation: ', error);
+                    console.error('Your browser encountered an error fetching a geolocation: ', error);
                 },
             );
         };
@@ -96,6 +108,8 @@ export default function Map({ latitude, longitude, zoom = 13 }: MapProps) {
                         </Popup>
                     </Marker>
                 )}
+
+                {home.length > 0 && <Marker position={[home[0], home[1]]} icon={HomeIcon}></Marker>}
             </LeafletMap>
         </>
     );
